42afd3c7e2c591a7b9f0fe99e77fc8ed840de509
ipa-nhg <nhg@ipa.fhg.de>
2017-06-02 09:12:30 +0200

    new kenny configuration

---
+++ robotino_bringup/env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export MY_CATKIN_WORKSPACE=~/catkin_ws
+source /etc/cob.bash.bashrc
+
+. /opt/ros/indigo/setup.sh
+
+if [ -e $MY_CATKIN_WORKSPACE/devel/setup.bash ]; then
+    source $MY_CATKIN_WORKSPACE/devel/setup.bash
+elif [ -e /u/squirrel/catkin_ws/devel/setup.bash ]; then
+    source /u/squirrel/catkin_ws/devel/setup.bash
+else
+    source /opt/ros/indigo/setup.bash
+fi
+
+export ROS_IP=`hostname -I | awk '{print $1}'`
+
+exec "$@"
+
