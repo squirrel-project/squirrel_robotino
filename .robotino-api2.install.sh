57e6d1cdeae611a7cac5b19b6314813a4913d100
ipa-nhg <nhg@ipa.fhg.de>
2017-07-27 16:19:06 +0200

    rename travis install file

--- .robotino-api2.install.sh
+++ .robotino-api2.install.sh
@@ -4,7 +4,3 @@ set -v
 
 wget http://doc.openrobotino.org/download/packages/amd64/robotino-api2_0.9.16_amd64.deb -O /tmp/robotino.deb
 dpkg -i /tmp/robotino.deb
-
-#MYARCH=$(uname -m)
-#apt-get install dpkg
-#dpkg -i $CATKIN_WORKSPACE/src/squirrel_driver/ft17_driver/ft17driver_0.0-1_${MYARCH}.deb
